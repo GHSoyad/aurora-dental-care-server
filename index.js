918a0afed639db26537a534bf013cde06c015b7b
GHSoyad <ghsoyad@gmail.com>
2022-11-18 15:52:24 +0600

    Added appointmentOptions get method

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7r6jn89.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+
+        const appointmentOptions = client.db('aurora-dental-care').collection('appointmentOptions');
+
+        app.get('/appointmentsOptions', async (req, res) => {
+            const query = {};
+            const result = await appointmentOptions.find(query).toArray();
+            res.send(result)
+        })
+
+    }
+    finally { }
+}
+
+run().catch(error => console.log(error))
 
 app.get('/', (req, res) => {
     res.send('Server is running!!')
