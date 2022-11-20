4ff574c981037c2c6df3f1cb6d661cf7ee5072da
GHSoyad <ghsoyad@gmail.com>
2022-11-20 21:37:58 +0600

    Added jwt and jwt validation

--- index.js
+++ index.js
@@ -3,11 +3,35 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
-
+const jwt = require('jsonwebtoken');
 
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized Access" })
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden Access" })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+app.get('/jwt', (req, res) => {
+    const email = req.query.email;
+    const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '24h' });
+    res.send({ token });
+})
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7r6jn89.mongodb.net/?retryWrites=true&w=majority`;
@@ -61,7 +85,7 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, async (req, res) => {
             const query = {};
             const cursor = usersCollection.find(query);
             const result = await cursor.toArray();
